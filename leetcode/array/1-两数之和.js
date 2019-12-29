8081f6c29a7be6b730688415e1c2ddce1af69e65
hankzhuo <zhuohangzhao1989@gmail.com>
2019-12-29 17:30:48 +0800

    update：数组 part 2

--- leetcode/array/1-两数之和.js
+++ leetcode/array/1-两数之和.js
@@ -1,6 +1,5 @@
 /**
 题目： 两数之和 
-leetcode 1
 
 说明：给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -11,7 +10,7 @@ leetcode 1
   所以返回 [0, 1]
 */
 
-// 解法1：O^2
+// 暴力解法：时间复杂度 O(n^2)、空间复杂度 O(n1)
 var twoSum = function(nums, target) {
     for (var i = 0; i < nums.length; i++) {
         for (var j = i + 1; j < nums.length; j++) {
@@ -22,19 +21,21 @@ var twoSum = function(nums, target) {
     }
 };
 
-// 解法2：精髓：相加等于目标值，等同于目标值减去任意一个值等于另一值
-//   1. 把 index 和 value 兑换，储存于一个对象中
-//   2. 借助对象，遍历这个数组，如果目标值 target - 遍历项，是否能找到，如果能找到，则保存这个下标，不能找到，则保存于对象中
+// 解法2：借助空间换时间的思维，先将每个元素的值和它的索引添加到 Hash 表中，然后第二次遍历，检查目标元元素 - 每个元素是否在 Hash 表中
+// 时间复杂度 O(n)、空间复杂度 O(n)
 var twoSum = function(nums, target) {
-  const ans = [];
-  const exist = {}
-  
-  for (let i = 0; i < nums.length; i++) {
-      if (typeof exist[target - nums[i]] !== 'undefined') {
-          ans.push(exist[target-nums[i]])
-          ans.push(i)
-      }
-      exist[nums[i]] = i;
-  }
-  return ans
+    let map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], i)
+    }
+    
+    for (let j = 0; j < nums.length; j++) {
+        let rest = target - nums[j];
+        // 相减得到的元素，是否在 hash 表中，且不等于自身
+        if (map.has(rest) && map.get(rest) !== j) {
+            return [j, map.get(rest)]
+        }
+    }
+    return []
 };
